a7e59abdaa747a57ba263f8b13ee7f5178c027a8
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-01-25 23:16:41 +0530

    Update and rename sample_config.py to config.py

--- haruka/config.py
+++ haruka/config.py
@@ -1,4 +1,4 @@
-if not __name__.endswith("sample_config"):
+
     import sys
     print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
           "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
